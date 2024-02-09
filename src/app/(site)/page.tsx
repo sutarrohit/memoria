6b50c8ca3eabc7757853b8006bb0a5b77c055d70
sutarrohit <rohitsutar082@gmail.com>
2024-02-09 19:49:56 +0530

    feat: add company name slider

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,6 +2,7 @@ import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from "@/lib/constants";
 import Banner from "../../../public/appBanner.png";
 
 const HomePage = () => {
@@ -13,16 +14,7 @@ const HomePage = () => {
           pill="✨ Your Workspace, Perfected"
           title="All-In-One Collaboration and Productivity Platform"
         />
-        <div
-          className="bg-white
-          p-[2px]
-          mt-6
-          rounded-xl
-          bg-gradient-to-r
-          from-primary
-          to-brand-primaryBlue
-          sm:w-[300px]"
-        >
+        <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
           <Button variant="outline" className="w-full rounded-[10px] p-6 text-2xl bg-background">
             Get Cypress Free
           </Button>
@@ -33,7 +25,52 @@ const HomePage = () => {
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
+
       {/* section2 */}
+      <section className="relative">
+        <div
+          className="overflow-hidden flex
+          after:content['']
+          after:dark:from-brand-dark
+          after:to-transparent
+          after:from-background
+          after:bg-gradient-to-l
+          after:right-0
+          after:bottom-0
+          after:top-0
+          after:w-20
+          after:z-10
+          after:absolute
+
+          before:content['']
+          before:dark:from-brand-dark
+          before:to-transparent
+          before:from-background
+          before:bg-gradient-to-r
+          before:left-0
+          before:top-0
+          before:bottom-0
+          before:w-20
+          before:z-10
+          before:absolute
+        "
+        >
+          {[...Array(2)].map((arr) => (
+            <div key={arr} className="flex flex-nowrap animate-slide">
+              {CLIENTS.map((client) => (
+                <div key={client.alt} className=" relative w-[200px] m-20 shrink-0 flex items-center">
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className="object-contain max-w-none"
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
