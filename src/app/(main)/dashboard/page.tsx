c11585adb9c9412a03b5081ea64d3385f933d9f7
sutarrohit <rohitsutar082@gmail.com>
2024-02-14 20:07:28 +0530

    feat: add workspace dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import db from "@/lib/supabase/db";
+// import { getUserSubscriptionStatus } from "@/lib/supabase/queries";
+import { redirect } from "next/navigation";
+import DashboardSetup from "@/components/dashboard-setup/dashboard-setup";
 
-const page = () => {
-  return <div>page</div>;
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  const workspace = await db.query.workspace.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  // const { data: subscription, error: subscriptionError } = await getUserSubscriptionStatus(user.id);
+  // if (subscriptionError) return;
+
+  if (!workspace)
+    return (
+      <div className="bg-background h-screen w-screen flex justify-center items-center">
+        {/* <DashboardSetup user={user} subscription={subscription} /> */}
+        <DashboardSetup />
+      </div>
+    );
+  redirect(`/dashboard/${workspace.id}`);
 };
 
-export default page;
+export default DashboardPage;
---
+++ src/components/dashboard-setup/dashboard-setup.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardSetup = () => {
+  return <div>DashboardSetup</div>;
+};
+
+export default DashboardSetup;
